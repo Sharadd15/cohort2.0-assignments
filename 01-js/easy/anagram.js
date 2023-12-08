8b94a0927ecef05babd98b313d9402cad7029aaa
sharad <sharadshukla429@gmail.com>
2023-12-08 07:58:26 +0000

    Solve Easy tasks

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length)
+    return false;
+  var accountArray = {};
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  for(i = 0; i < str1.length; ++i)
+  {
+    if(str1[i] in accountArray)
+      ++accountArray[str1[i]];
+    else
+      accountArray[str1[i]] = 1;
+  }
 
+  for(i = 0;i < str2.length; ++i)
+  {
+    if( str2[i] in accountArray && accountArray[str2[i]] != 0)
+      --accountArray[str2[i]];
+
+    else
+      return false;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
