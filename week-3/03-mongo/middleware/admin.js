decf4711be79e4b8e40775470f444c574b5a3e14
Sharad Shukla <sharadshukla429@gmail.com>
2023-12-24 14:25:38 +0000

    Solve Part3

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,27 @@
+const Admin = require('../db/index').Admin;
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const userName = req.headers.username;
+    const password = req.headers.password;
+    Admin.findOne({username: userName})
+         .exec()
+         .then((data) => {
+            if(data.password === password)
+                next();
+            else
+                res.status(404).json({
+                    message: "Invalid UserName or password."
+            });
+        })
+        .catch(err =>
+        {
+            res.status(500).json({
+                message: "Internal server error."
+            });
+            console.log(err);
+        });
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,66 @@
 const { Router } = require("express");
+const { Admin, Course } = require("../db/index");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
-});
+    const userName = req.body.username;
+    const password = req.body.password;
+    //console.log(req.body);
+    Admin.findOne({username: userName})
+        .exec()
+        .then((data) => {
+            if(data)
+                res.status(409).json({message: 'User already exists.'});
+            else
+            {
+                Admin.create({
+                    username: userName,
+                    password: password
+                });
+                res.json({message: 'User created successfully'});
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Internal server error." });
+            console.log(err);
+        });
+}); 
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    Course.create({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        imageLink: req.body.imageLink,
+    })
+    .then(data => {
+        res.json({
+            message: 'Course created successfully', 
+            courseId: data._id
+        });
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Failed to create course internal server error."});
+    });
+    
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({}).exec()
+                    .then(data =>
+                    {
+                        if(data)
+                            res.json(data);
+                    })
+                    .catch(err => {
+                        res.status(500).json({ message: "Internal server error."});
+                        console.log(err);
+                    });
 });
 
 module.exports = router;
